7345c92490b3efd5d04114022c7b7e3c3c5ced2b
AxonDesigns <acciol400@gmail.com>
2024-03-20 22:16:07 -0500

    Initial commit

---
+++ src/main/kotlin/com/sena/inventory_manager/InventoryManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.sena.inventory_manager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InventoryManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<InventoryManagerApplication>(*args)
+}
---
+++ src/test/kotlin/com/sena/inventory_manager/InventoryManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sena.inventory_manager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InventoryManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
